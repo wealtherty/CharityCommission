b627726ab45e9a601cc700ed8c2429d349404dd7
wealtherty <yw8cq3@pm.me>
2023-08-31 14:24:32 +0100

    Using UserScrets to populate SubscriptionKey

--- src/CharityCommission.IntegrationTests/Startup.cs
+++ src/CharityCommission.IntegrationTests/Startup.cs
@@ -8,22 +8,17 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddSingleton(_ => new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", false, false)
             .AddUserSecrets(typeof(Startup).Assembly)
             .Build());
         
         services.AddSingleton(provider =>
         {
-            var configuration = provider.GetRequiredService<IConfigurationRoot>();
-            var section = configuration.GetSection(typeof(CharityCommissionClient).Namespace);
+            var configurationRoot = provider.GetRequiredService<IConfigurationRoot>();
+
+            var section = configurationRoot.GetSection(typeof(CharityCommissionClient).Namespace);
             return section.Get<CharityCommissionSettings>();
         });
-        services.AddSingleton(provider =>
-        {
-            var settings = provider.GetRequiredService<CharityCommissionSettings>();
-
-            return new CharityCommissionClient(settings);
-        });
+        services.AddSingleton<CharityCommissionClient>();
         services.AddSingleton<Fixture>();
     }
 }
